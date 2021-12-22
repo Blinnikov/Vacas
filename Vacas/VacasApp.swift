c6efe3432ba266b43b2844644724420bb22c5300
Igor Blinnikov <igor.blinnikov@gmail.com>
2021-12-23 00:37:00 +0300

    feat(vacas): init

---
+++ Vacas/VacasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacasApp.swift
+//  Vacas
+//
+//  Created by Igor Blinnikov on 23.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct VacasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
