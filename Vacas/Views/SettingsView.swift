1820b7d3202fcc07d868ec005ad51fb86e643330
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-01-04 00:29:07 +0300

    feat(settings): very basic UI draft

--- Vacas/Views/SettingsView.swift
+++ Vacas/Views/SettingsView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct SettingsView: View {
+  @State var appearance: Int = 2
+  @State var hireDate: Date = Date(timeIntervalSince1970: 0)
+  
   var body: some View {
-    VStack {
-      Text("Settings View will be there")
-        .font(.headline)
-      Text("You just need to wait a little bit")
-        .font(.subheadline)
-        .foregroundColor(.secondary)
+    List {
+      // TODO: Add Navigation stack and use navigationTitle?
+      // or something similar
+      Text("Settings")
+        .font(.largeTitle)
+      
+      Section("Appearance") {
+        Picker("Appearance", selection: $appearance) {
+          Text("Light").tag(0)
+          Text("Dark").tag(1)
+          Text("System").tag(2)
+        }
+        .pickerStyle(SegmentedPickerStyle())
+        
+        HStack {
+          Text("Tint color")
+          Spacer()
+          Image(systemName: "circle.fill").foregroundColor(Color.selection)
+          Image(systemName: "circle.fill").foregroundColor(Color.plum)
+          Image(systemName: "circle.fill").foregroundColor(Color.peach)
+        }
+      }
+      
+      Section {
+        DatePicker("Hire Date", selection: $hireDate, displayedComponents: .date)
+        
+        Text("Vacation days per year - 28") // Stepper
+        
+        Text("Days that can be compensated - 3") // Stepper
+        
+        Text("Service to fetch holidays from") // CoppClark?
+      }
     }
   }
 }
