7967aa1d60ac0a113e5d95fca265284fd4076601
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2022-03-01 19:41:27 +0300

    feat(vacas): add tests bundle

---
+++ Vacas/Model/DateService.swift
@@ -0,0 +1,14 @@
+//
+//  DateService.swift
+//  Vacas
+//
+//  Created by Igor Blinnikov on 01.03.2022.
+//
+
+import Foundation
+
+struct DateService {
+  func getToday() -> Date {
+    return Date()
+  }
+}
