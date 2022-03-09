478b7dcf10d84f47f28da3ab0f0e0a16a577c601
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-03-09 22:50:38 +0300

    chore: cleanup tests code

--- VacasTests/VacasTests.swift
+++ VacasTests/VacasTests.swift
@@ -11,7 +11,6 @@ import XCTest
 class VacasTests: XCTestCase {
   let dateService = DateService()
   
-  
   override func setUpWithError() throws {
     // Put setup code here. This method is called before the invocation of each test method in the class.
   }
@@ -42,13 +41,6 @@ class VacasTests: XCTestCase {
     // Any test you write for XCTest can be annotated as throws and async.
     // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
     // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
   }
-  
-  func testPerformanceExample() throws {
-    // This is an example of a performance test case.
-    measure {
-      // Put the code you want to measure the time of here.
-    }
-  }
-  
 }
