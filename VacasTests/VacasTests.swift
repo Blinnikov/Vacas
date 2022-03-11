3acc20756e277d316a3de2c2cf2d92cf0d5ff03c
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-03-11 14:45:11 +0300

    chore: remove example test

--- VacasTests/VacasTests.swift
+++ VacasTests/VacasTests.swift
@@ -42,13 +42,4 @@ class VacasTests: XCTestCase {
     let count = dateService.workingDays(between: march1, and: march8)
     XCTAssertEqual(count, 5)
   }
-  
-  func testExample() throws {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    // Any test you write for XCTest can be annotated as throws and async.
-    // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-    // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    
-  }
 }
