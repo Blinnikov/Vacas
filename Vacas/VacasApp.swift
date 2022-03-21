b1e9bdd9ff1f0cc0d388f455ddc394d847e39d66
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-03-21 19:41:02 +0300

    feat(statistics): initialize statistics view model in app root

--- Vacas/VacasApp.swift
+++ Vacas/VacasApp.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 @main
 struct VacasApp: App {
-  @StateObject var settingsStore = SettingsStore(named: "Default")
+  @StateObject var settingsStore: SettingsStore
+  @StateObject var statisticsViewModel: StatisticsViewModel
+  
+  init() {
+    let store = SettingsStore(named: "Default")
+    
+    self._settingsStore = StateObject(wrappedValue: store)
+    self._statisticsViewModel = StateObject(wrappedValue: StatisticsViewModel(with: store))
+  }
   
   var body: some Scene {
     WindowGroup {
       ContentView()
         .preferredColorScheme(settingsStore.settings.appearance.getColorScheme())
         .environmentObject(settingsStore)
+        .environmentObject(statisticsViewModel)
     }
   }
 }
