9aa94a320db44f104fbf4d23295ead821f0104ca
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-04-07 00:49:40 +0300

    chore(statistics): add vacations circle draft

--- Vacas/Views/StatisticsView.swift
+++ Vacas/Views/StatisticsView.swift
@@ -11,24 +11,62 @@ struct StatisticsView: View {
   @EnvironmentObject var statistics: StatisticsViewModel
   
   var body: some View {
-    VStack {
-      HStack {
-        Text("Developer Info")
-          .font(.title)
-          .padding()
+    GeometryReader { proxy in
+      VStack {
+        HStack {
+          // TODO: Get rid of this title
+          // Make some cool representative start screen
+          Text("Statistics")
+            .font(.title)
+            .padding()
+          
+          Spacer()
+        }
         
-        Spacer()
+        listItem(title: "Working days this year", statistics.workingDaysThisYear)
+        
+        listItem(title: "Vacation days gained", statistics.vacationDaysGained)
+        
+        listItem(title: "Vacation days spent this year", statistics.vacationDaysSpentThisYear)
+        
+        listItem(title: "Vacation days left this year", statistics.vacationDaysLeftThisYear)
+        
+        Divider()
+        
+        circle(in: proxy.size)
+        
+        Divider()
+        
+        listItem(title: "Width", Int(proxy.size.width))
+        
+        listItem(title: "Height", Int(proxy.size.height))
+        
+//        Spacer()
       }
+    }
+    .border(Color.red, width: 2)
+  }
+  
+  private func circle(in size: CGSize) -> some View {
+    let CircleRatio = 0.5
+    let CircleLineWidth: CGFloat = 15
+    let CircleShadow: CGFloat = 10
+    let CircleColor = Color.green
+    let CircleFontSize: CGFloat = 60
+    
+    let circleWidth = CircleRatio * size.width
+    
+    let overlay = Circle().stroke(CircleColor, lineWidth: CircleLineWidth).shadow(radius: CircleShadow)
+    return ZStack {
+      Circle()
+        .fill(.clear)
+        .frame(width: circleWidth, height: circleWidth)
+        .overlay(overlay)
       
-      listItem(title: "Working days this year", statistics.workingDaysThisYear)
-      
-      listItem(title: "Vacation days gained", statistics.vacationDaysGained)
-      
-      listItem(title: "Vacation days spent this year", statistics.vacationDaysSpentThisYear)
-      
-      listItem(title: "Vacation days left this year", statistics.vacationDaysLeftThisYear)
-      
-      Spacer()
+      // FIX: Shows wrong count on Main tab
+      Text("\(statistics.vacationDaysLeftThisYear)")
+        .foregroundColor(CircleColor)
+        .font(.system(size: CircleFontSize))
     }
   }
   
