e69768aa577fbe49267d2a38e4c73ea9e9645c64
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-04-13 01:22:03 +0300

    feat(add record): type selector draft

--- Vacas/Views/AddScheduleRecordView.swift
+++ Vacas/Views/AddScheduleRecordView.swift
@@ -10,27 +10,15 @@ import SwiftUI
 struct AddScheduleRecordView: View {
   @Binding var record: ScheduleRecord
   
+  @State var type = ScheduleChangeType.timeOff(.holiday)
+  
   var body: some View {
     VStack(alignment: .leading, spacing: 10) {
-      Text(record.date, style: .time)
-      
-      Picker(selection: $record.type){
-        Text("Overtime").tag(ScheduleChangeType.overtime)
-        Text("Vacation").tag(ScheduleChangeType.timeOff(.vacation))
-        Text("Holiday").tag(ScheduleChangeType.timeOff(.holiday))
-        Text("Parental Leave").tag(ScheduleChangeType.timeOff(.parentalLeave))
-        Text("Personal Time").tag(ScheduleChangeType.timeOff(.personalTime))
-        Text("Sick Leave").tag(ScheduleChangeType.timeOff(.sickLeave))
-      } label: {
-        Image(systemName: "plus")
+      HStack {
+        Text(record.date, style: .time)
+        Spacer()
+        typeSelector
       }
-      .pickerStyle(MenuPickerStyle())
-      .accentColor(.white)
-      .font(.title.bold())
-//      .tint(.red)
-      .padding(4)
-      .background(.gray)
-      .cornerRadius(15)
       
       TextField("Enter a title", text: $record.title)
         .font(.title2.bold())
@@ -41,6 +29,26 @@ struct AddScheduleRecordView: View {
     .background(
       Color.bloodOrange
     )
+  }
+  
+  var typeSelector: some View {
+    Menu {
+      Picker(selection: $type, label: Image(systemName: "plus")){
+        Text("Overtime").tag(ScheduleChangeType.overtime)
+        Text("Vacation").tag(ScheduleChangeType.timeOff(.vacation))
+        Text("Holiday").tag(ScheduleChangeType.timeOff(.holiday))
+        Text("Parental Leave").tag(ScheduleChangeType.timeOff(.parentalLeave))
+        Text("Personal Time").tag(ScheduleChangeType.timeOff(.personalTime))
+        Text("Sick Leave").tag(ScheduleChangeType.timeOff(.sickLeave))
+      }
+    } label: {
+      let overlay = Circle().stroke(.white, lineWidth: 2).shadow(radius: 2)
+      
+      Circle()
+        .fill(ScheduleRecord(title: record.title, date: record.date, type: type).backgroundColor)
+        .frame(width: 24, height: 24)
+        .overlay(overlay)
+    }
   }
 }
 
