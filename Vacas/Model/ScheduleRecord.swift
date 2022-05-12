1d5930b48161c8d92c9bc3c7db7561e811aefc86
Igor Blinnikov <igor.blinnikov@gmail.com>
2022-05-12 12:39:18 +0300

    feat(records store): make ScheduleRecord Codable

--- Vacas/Model/ScheduleRecord.swift
+++ Vacas/Model/ScheduleRecord.swift
@@ -7,19 +7,19 @@
 
 import Foundation
 
-struct ScheduleRecord: Identifiable {
-  let id = UUID()
+struct ScheduleRecord: Identifiable, Codable {
+  var id = UUID()
   var title: String
   let date: Date
   var type: ScheduleChangeType
 }
 
-enum ScheduleChangeType: Hashable {
+enum ScheduleChangeType: Hashable, Codable {
   case overtime
   case timeOff(TimeOffType)
 }
 
-enum TimeOffType {
+enum TimeOffType: Codable {
   case vacation
   case parentalLeave
   case sickLeave
